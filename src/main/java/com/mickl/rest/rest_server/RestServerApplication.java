3ce44e78ee452ebe4e8d7c9e4188ad8560dcc7bc
Michał Celniak <michal.celniak@ttpsc.pl>
2017-11-11 19:32:47 +0100

    refactor

--- src/main/java/com/mickl/rest/rest_server/RestServerApplication.java
+++ src/main/java/com/mickl/rest/rest_server/RestServerApplication.java
@@ -3,7 +3,6 @@ package com.mickl.rest.rest_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableMongoRepositories
--- src/main/java/com/mickl/rest/rest_server/security/config/MongoAuthenticationManager.java
+++ src/main/java/com/mickl/rest/rest_server/security/config/MongoAuthenticationManager.java
@@ -2,17 +2,19 @@ package com.mickl.rest.rest_server.security.config;
 
 import com.mickl.rest.rest_server.security.model.User;
 import com.mickl.rest.rest_server.security.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
-public class MongoAuthenticationManager implements AuthenticationManager {
+@Slf4j
+class MongoAuthenticationManager implements AuthenticationManager {
 
-    private UserService userService;
+    private final UserService userService;
 
-    public MongoAuthenticationManager(UserService userService) {
+    MongoAuthenticationManager(UserService userService) {
         this.userService = userService;
     }
 
@@ -24,13 +26,14 @@ public class MongoAuthenticationManager implements AuthenticationManager {
                 .orElseThrow(() -> new BadCredentialsException("User Not Found!"));
 
         if (passwordsMatch(password, user)) {
+            log.trace("User logged in: ".concat(name));
             return new UsernamePasswordAuthenticationToken(
                     authentication.getPrincipal(),
                     authentication.getCredentials(),
                     user.getAuthorities());
         }
 
-        throw new BadCredentialsException("Password incorrect!");
+        throw new BadCredentialsException("User or password incorrect!");
     }
 
     private boolean passwordsMatch(String password, User user) {
